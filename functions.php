57a4646e686c3aaf237a61286baa986e1ebfe332
saschahaarup <90177372+saschahaarup@users.noreply.github.com>
2023-06-01 14:37:23 +0200

    Upload af website

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This is the child theme for Astra theme, generated with Generate Child Theme plugin by catchthemes.
+ *
+ * (Please see https://developer.wordpress.org/themes/advanced-topics/child-themes/#how-to-create-a-child-theme)
+ */
+add_action( 'wp_enqueue_scripts', 'child_theme_egmont_enqueue_styles' );
+function child_theme_egmont_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+/*
+ * Your code goes below
+ * Hej
+ */
